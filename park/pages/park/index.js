ca55e37dc88b1fb863d3d51515d176586bee8179
Alejandro Baez <nativo.alejandro@gmail.com>
2021-05-30 10:38:15 -0500

    added base styles for park

--- park/pages/park/index.js
+++ park/pages/park/index.js
@@ -1,13 +1,17 @@
 import { useRouter } from 'next/router'
 import { usePark } from '/utils/hooks'
+import styles from './Park.module.css'
 
 export default function Park(props) {
 	// const { dogs, error } = usePark()
 	const router = useRouter()
 	return (
-		<div>
+		<div className={styles.container}>
 			<h1>Parque Laureles</h1>
+			<div className={styles.park}>
+				
+			</div>
 			<button onClick={() => router.push('/menu')}>menu</button>
 		</div>
-		)
+	)
 }
