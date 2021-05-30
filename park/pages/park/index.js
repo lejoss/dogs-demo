5dfbe270c9385377cb591529e1b9016ad617e02e
Alejandro Baez <nativo.alejandro@gmail.com>
2021-05-30 11:38:10 -0500

    added title styles

--- park/pages/park/index.js
+++ park/pages/park/index.js
@@ -1,15 +1,23 @@
 import { useRouter } from 'next/router'
 import { usePark } from '/utils/hooks'
+import Title from '/components/Title'
 import styles from './Park.module.css'
 
 export default function Park(props) {
-	// const { dogs, error } = usePark()
+	const { dogs, error } = usePark()
 	const router = useRouter()
 	return (
 		<div className={styles.container}>
-			<h1>Parque Laureles</h1>
+			<Title>Parque Laureles</Title>
 			<div className={styles.park}>
-				
+				<ul className={styles.ul}>
+					{
+						dogs
+							? dogs.map((dog, i) => <li className={styles.li} key={i}>{dog.name}</li>)
+							: null
+					}
+				</ul>
+
 			</div>
 			<button onClick={() => router.push('/menu')}>menu</button>
 		</div>
