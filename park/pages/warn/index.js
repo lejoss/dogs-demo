6626d5c49a6d11e3d8edf968bafee34cd3665428
Alejandro Baez <nativo.alejandro@gmail.com>
2021-05-31 09:26:44 -0500

    update

--- park/pages/warn/index.js
+++ park/pages/warn/index.js
@@ -1,12 +1,13 @@
 import { useWarn } from '/utils/hooks'
-import styles from './Warning.module.css'
+import { Title } from '/components'
+import styles from '/styles/globals.css'
 
 export default function Warning(props) {
 	useWarn()
 	return (
-		<div>
-			<h1>Advertencia</h1>
-			<p>porfavor activa las notificaciones en este sitio para usar la aplicacion</p>
+		<div style={styles.container}>
+			<Title>Advertencia</Title>
+			<p>porfavor activa las notificaciones en este sitio para usar la aplicacion y recarga la app</p>
 		</div>
 	)
 }
