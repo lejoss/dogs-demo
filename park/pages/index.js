3616c272355b4ab67f37dfbd0ff89adeb640dd1e
Alejandro Baez <nativo.alejandro@gmail.com>
2021-06-03 15:58:30 -0500

    playing with styles in home page

--- park/pages/index.js
+++ park/pages/index.js
@@ -30,9 +30,13 @@ export default function Home(props) {
 
   return (
     <div className={styles.container}>
+      <div className={styles.hero}>
+        <div style={{ padding: '5em 0 5em 2.2em' }}>
+          <div className={styles.title}>Mis Mascotas</div>
+        </div>
+      </div>
       {userDogs && (
         <div className={styles.pets}>
-          <Title style={{ textAlign: 'left' }}>Mis Mascotas</Title>
           <ul className={styles.ul}>
             {userDogs && userDogs.length && dogs.map((dog, i) => <li className={styles.li} key={i}>{dog.name}</li>)}
           </ul>
@@ -63,31 +67,15 @@ export default function Home(props) {
         </div>
       )}
 
-
-      <div className={styles.btn__group}>
-        {!userDogs && (
-          <>
-            <h3 style={{ textAlign: 'center' }}>No tienes perros registrados</h3>
-          </>
-        )}
-
-        <Modal>
-          <ModalOpenButton>
-            <Button>Informacion</Button>
-          </ModalOpenButton>
-          <ModalContents style={{ width: '83vw' }} aria-label="Modal label (for screen readers)">
-            <h3>App Info</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-            </p>
-            <br />
-            <ModalDismissButton>
-              <Button>Cerrar</Button>
-            </ModalDismissButton>
-          </ModalContents>
-        </Modal>
-
-        <Link href="/dog">Registrar mascota</Link>
+      <div className={styles.park}>
+        {activeDogs && activeDogs.length
+          ? <span>{`${activeDogs.length} Perro en el parque`}</span>
+          : <span>{'No hay perros en el parque'}</span>
+        }
+        <br />
+        <br />
+        <Link className={styles.btn} href="/park">Ver Parque</Link>
+        <div style={{ marginTop: '.6em'}}></div>
 
         {dogs && !isUserVisiting &&
           (
@@ -109,13 +97,41 @@ export default function Home(props) {
             </Modal>
           )
         }
-
-        {activeDogs && activeDogs.length
-          ? <span>{`${activeDogs.length} Perro en el parque`}</span>
-          : <span>{'No hay perros en el parque'}</span>
-        }
-        <Link className={styles.btn} href="/park">Ver Parque</Link>
       </div>
+
+      <br />
+      <br />
+
+
+      <div className={styles.btn__group}>
+        {!userDogs && (
+          <>
+            <h3 style={{ textAlign: 'center' }}>No tienes perros registrados</h3>
+          </>
+        )}
+
+        <Link href="/dog">Registrar mascota</Link>
+
+      </div>
+
+      <div className={styles.info}>
+        <Modal>
+          <ModalOpenButton>
+            <Button>Informacion</Button>
+          </ModalOpenButton>
+          <ModalContents style={{ width: '83vw' }} aria-label="Modal label (for screen readers)">
+            <h3>App Info</h3>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            </p>
+            <br />
+            <ModalDismissButton>
+              <Button>Cerrar</Button>
+            </ModalDismissButton>
+          </ModalContents>
+        </Modal>
+      </div>
+
     </div>
   )
 }
