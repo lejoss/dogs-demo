239610c52b91d4e6dff2fa9f4c1001fb78cc0f48
Alejandro Baez <nativo.alejandro@gmail.com>
2021-06-06 16:05:29 -0500

    added styles to registe dog view

--- park/pages/dog/index.js
+++ park/pages/dog/index.js
@@ -61,8 +61,7 @@ export default function DogForm(props) {
 
 	return (
 		<div style={{ padding: '2em', height: '100vh', overflow: 'hidden' }}>
-			<Title>Registrar Mascota</Title>
-			<form style={{ marginTop: '2em' }} onSubmit={handleSubmit} {...props}>
+			<form style={{ color: '#005005', background: 'white', marginTop: '2em', padding: '2em', borderRadius: 10, boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)' }} onSubmit={handleSubmit} {...props}>
 				<div>
 					<DogInput
 						type="text"
@@ -92,7 +91,7 @@ export default function DogForm(props) {
 				</div>
 				<br />
 				<br />
-				<Button style={{ background: '#d81b60' }} type="submit">Registrar Mascota</Button>
+				<Button style={{ background: 'transparent', color: '#005005' }} type="submit">Registrar</Button>
 			</form>
 			<br />
 			<Link href="/">VOLVER AL INICIO</Link>
