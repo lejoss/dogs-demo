0031ff14b32b9cb9082ea04a2ccb256d6125393d
Alejandro Baez <nativo.alejandro@gmail.com>
2021-06-10 11:06:29 -0500

    removed padding from card

--- park/components/RegisterDog.js
+++ park/components/RegisterDog.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function RegisterDog(props) {
 	return (
 		<section>
-			<Card variant="accent">
+			<Card variant="ligth">
 				<h3>{props.children || props.text}</h3>
 				<Link href="/dog">{props.buttonText || 'Registrar'}</Link>
 			</Card>
