caeb73e80e7d61f2adcc613992f2767cc4a073a9
Alejandro Baez <nativo.alejandro@gmail.com>
2021-06-20 10:33:32 -0500

    Update sw.js

--- park/public/sw.js
+++ park/public/sw.js
@@ -22,11 +22,25 @@ self.addEventListener('push', event => {
             //icon: '/public/grass.svg'
         })
     )
-});
+})
 
 self.addEventListener('notificationclick', function (event) {
-    event.notification.onclick = function (event) {
-        event.preventDefault(); // Previene al buscador de mover el foco a la pestaña del Notification
-        window.open('https://laureles.vercel.app', '_blank');
-    }
+    let url = 'https://laureles.vercel.app'
+    event.notification.close(); // Android needs explicit close.
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(windowClients => {
+            // Check if there is already a window/tab open with the target URL
+            for (var i = 0; i < windowClients.length; i++) {
+                var client = windowClients[i];
+                // If so, just focus it.
+                if (client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            // If not, then open the target URL in a new window/tab.
+            if (clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    )
 })
